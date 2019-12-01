a156bf9b2accf1cf53e698da34ecb25dec03630b
Bala <bala@gmail.com>
2019-12-01 21:39:57 +0530

    test commit

---
+++ bala-project1/src/main/java/bala-project1/App.java
@@ -0,0 +1,13 @@
+package bala-project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
